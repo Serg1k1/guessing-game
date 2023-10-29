95e45f7588bd967a7d5119fe35c7163485d46ed8
Sergey <kindratsergij60@gmail.com>
2023-10-29 15:32:32 +0200

    feat: done task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,33 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+    this.number = [];
+    this.left = 0;
+    this.right;
+    this.center;
+    }
     setRange(min, max) {
-
+        for (let i = min; i <= max; i++) {
+            this.number.push(i);
+        }
+        this.right = this.number[this.number.length - 1];
+        return this.number;
     }
 
     guess() {
-
+        this.center = Math.round((this.left + this.right) / 2);
+        return this.center;
     }
 
     lower() {
-
+        this.right = this.center;
+        this.center = Math.round((this.left + this.right) / 2);
+        return this.center
     }
 
     greater() {
-
+        this.left = this.center;  
+        this.center = Math.round((this.left + this.right) / 2);  
+        return this.center;
     }
 }
 
